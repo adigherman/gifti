a92958745fda4829ddf8233cb9a11520b37ce72b
Adi G <adig@jhu.edu>
2021-01-12 16:19:34 -0500

    inject tic function

--- tic.R
+++ tic.R
@@ -1,2 +1,10 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks(args=c("--as-cran","--install-args=--build"))
+
+hello<-function()
+{
+  message('Hello World');
+}
+
+get_stage("before_install") %>%
+  add_step(hello())
