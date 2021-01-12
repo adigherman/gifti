7c8c523ebd81ccae2adb46d0e533b85e034b42e6
Adi G <adig@jhu.edu>
2021-01-12 16:49:53 -0500

    message

--- tic.R
+++ tic.R
@@ -3,5 +3,5 @@ do_package_checks(args=c("--as-cran","--install-args=--build"))
 
 get_stage("before_install") %>%
   add_code_step(message("Listing folder and files")) %>%
-  add_code_step(getwd()) %>%
-  add_code_step(list.files())
+  add_code_step(message(getwd())) %>%
+  add_code_step(message(list.files()))
