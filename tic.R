71c67a04a6261b8c069413a7d48f8df0f6351770
Adi G <adig@jhu.edu>
2021-01-12 16:34:11 -0500

    simplify

--- tic.R
+++ tic.R
@@ -1,10 +1,5 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks(args=c("--as-cran","--install-args=--build"))
 
-hello<-function()
-{
-  message('Hello World');
-}
-
 get_stage("before_install") %>%
-  add_step(hello())
+  add_code_step(message("Hellow World it's me Mario!"))
