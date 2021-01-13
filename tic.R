4c94ac3f027842e2be1c9b7f9d3cf1eb44e61a31
adigherman <adig@jhu.edu>
2021-01-13 16:55:51 -0500

    testing remotes install

--- tic.R
+++ tic.R
@@ -1,9 +1,21 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks(args=c("--as-cran","--install-args=--build"))
 
-get_stage("before_install") %>%
-  add_code_step(message("Listing folder and files")) %>%
-  add_code_step(message(getwd())) %>%
-  add_code_step(message(list.files())) %>%
-  add_code_step(message("Installing ITKR binaries")) %>%
-  add_code_step(ghtravis::install_remote_binaries(check_r_version = TRUE, force_sha = FALSE, remotes = c("neuroconductor/ITKR")))
+# get remotes and try to install binaries
+remotes = get_remotes()
+
+if(!is.null(remotes)) {
+  for(remote in remotes) {
+    remote_info <- parse_remotes(remote)
+    github_user <- sapply(remote_info, `[[`, "username")
+    repo <- sapply(remote_info, `[[`, "repo")
+    slug <- paste0(paste0(github_user,"/",repo))
+
+    if(!is.null(github_user) && !is.null(repo)) {
+      get_stage("before_install") %>%
+        add_code_step(message(paste0("Installing ",repo," binaries"))) %>%
+        add_code_step(ghtravis::install_remote_binaries(check_r_version = TRUE, force_sha = FALSE, remotes = c(slug)))
+    }
+  }
+}
+
