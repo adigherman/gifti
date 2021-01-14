716701e3c214b5c7d6254f82ed90c47dd70e3134
adigherman <adig@jhu.edu>
2021-01-14 09:12:13 -0500

    outside

--- tic.R
+++ tic.R
@@ -15,6 +15,10 @@ HelloWorlds <- R6Class(
   )
 )
 
+step_hello_world <- function() {
+  HelloWorld$new()
+}
+
 if(!is.null(remotes)) {
   for(remote in remotes) {
     remote_info <- ghtravis::parse_remotes(remote)
@@ -22,12 +26,12 @@ if(!is.null(remotes)) {
     repo <- sapply(remote_info, `[[`, "repo")
     slug <- paste0(paste0(github_user,"/",repo))
 
-    if(!is.null(github_user) && !is.null(repo)) {
-      get_stage("before_install") %>%
-        add_code_step(HelloWorlds$new())
+#    if(!is.null(github_user) && !is.null(repo)) {
+#      get_stage("before_install") %>%
+#        add_code_step(HelloWorlds$new())
 #        add_code_step(message(paste0("Installing ","repo"," binaries"))) %>%
 #        add_code_step(ghtravis::install_remote_binaries(check_r_version = TRUE, force_sha = FALSE, remotes = c("slug")))
-    }
+#    }
   }
 }
 
